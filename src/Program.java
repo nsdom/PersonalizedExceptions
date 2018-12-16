6a9a4ab7dcccfa5cc5968b368161436f36f37d04
Nicolá Domingues <nsdomingues44@gmail.com>
2018-12-16 11:34:08 +0000

    Bad Solution

--- src/Program.java
+++ src/Program.java
@@ -8,50 +8,47 @@ import entities.Reservation;
 public class Program {
 
 	public static void main(String[] args) throws ParseException {
-		
+
 		Scanner sc = new Scanner(System.in);
-		SimpleDateFormat sdf= new SimpleDateFormat("dd/MM/yyyy");
-		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
 		// Enter data Reservation DATA:
 		System.out.print("Room number: ");
 		int roomNumber = sc.nextInt();
-		
+
 		System.out.print("Check-in date (dd/MM/yyyy) ");
 		Date checkIn = sdf.parse(sc.next());
-		
+
 		System.out.print("Check-out date (dd/MM/yyyy) ");
 		Date checkOut = sdf.parse(sc.next());
-		
+
 		if (!checkOut.after(checkIn)) {
-			System.out.println("Error in reservation: Check-out date must be after check-in date.");		
-		}
-		else {
+			System.out.println("Error in reservation: Check-out date must be after check-in date.");
+		} else {
 			Reservation reservation = new Reservation(roomNumber, checkIn, checkOut);
 			System.out.println("Reservation:" + reservation);
-			
+
 			System.out.println();
-			
+
 			System.out.println("Enter data to update reservation: ");
 			System.out.print("Check-in date (dd/MM/yyyy) ");
 			checkIn = sdf.parse(sc.next());
-			
+
 			System.out.print("Check-out date (dd/MM/yyyy) ");
 			checkOut = sdf.parse(sc.next());
-			Date now = new Date();
-			if (checkIn.before(now) || checkOut.before(now)) {
-				System.out.println("Error in reservation: Reservation dates for update must be future dates.");
-			}
-			else if (!checkOut.after(checkIn)) {
-				System.out.println("Error in reservation: Check-out date must be after check-in date.");		
+
+			String error = reservation.updateDates(checkIn, checkOut);
+			if (error != null) {
+				System.out.println("Error in reservation: " + error);
 			}
 			else {
-				reservation.updateDates(checkIn, checkOut);
 				System.out.println("Reservation:" + reservation);
+
 			}
-
-
-			
 		}
+		
+		
+		sc.close();
 	}
 
 }
